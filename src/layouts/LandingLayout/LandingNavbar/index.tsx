27110fb7bf5069ec492eb92262cb40907da19897
afauzitkj2-ai <a.fauzitkj2@gmail.com>
2025-08-15 14:59:06 +0700

    ganti nama cv

--- src/layouts/LandingLayout/LandingNavbar/index.tsx
+++ src/layouts/LandingLayout/LandingNavbar/index.tsx
@@ -4,7 +4,7 @@ export default function LandingNavbar() {
   return (
     <div className="bg-base-100 bg-opacity-60 backdrop-blur py-4">
       <div className="max-w-screen-lg flex justify-between mx-auto md:px-20 px-5">
-        <a className="text-xl font-bold">Kurniawan Try Yudha</a>
+        <a className="text-xl font-bold">fauzi gangsar</a>
         <ToggleThemeNavbar />
       </div>
     </div>
