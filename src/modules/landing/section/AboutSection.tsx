21e304ccff99e8a38df44cb64c4aade6e85fb372
afauzitkj2-ai <a.fauzitkj2@gmail.com>
2025-08-15 15:06:16 +0700

    aboutsection

--- src/modules/landing/section/AboutSection.tsx
+++ src/modules/landing/section/AboutSection.tsx
@@ -5,17 +5,7 @@ export default function AboutSection() {
         <p className="md:w-2/6 text-xl font-semibold md:text-left text-center">
           About
         </p>
-        <h3 className="md:w-4/6 text-sm text-justify leading-6">
-          Full stack Web Developer with over 3 years of experience designing and
-          building scalable web applications across digital media, UAV control
-          systems, and enterprise platforms. Skilled in Laravel, TypeScript
-          (React, Vue), real-time communication protocols like Socket.IO and
-          MQTT, and implementing secure authentication methods. Experienced in
-          improving user experience through reusable UI components and features
-          integrated with Google Analytics. Passionate about clean architecture,
-          CI/CD automation, and delivering robust solutions in agile
-          environments. Strong collaborator committed to driving impactful
-          projects from concept to deployment.
+        <h3 className="md:w-4/6 text-sm text-justify leading-6">my name is gangsar wijaya and my friend is fauzi dwi siswamto.
         </h3>
       </div>
     </section>
